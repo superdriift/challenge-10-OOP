d3883ca21dbe64c07472301a0d5c4921703db36e
Julian Longmire <julian@Julians-MacBook-Pro.local>
2023-01-01 19:43:27 -0800

    submission file

---
+++ library/intern.js
@@ -0,0 +1,30 @@
+const Employee = require('./employee')
+
+class Intern extends Employee {
+    constructor (name, id, email, school) {
+        super(name, id, email)
+        this.school = school;
+    }
+    getName() {
+        return(this.name)
+    }
+
+    getId() {
+        return(this.id)
+    }
+
+    getEmail() {
+        return(this.email)
+    }
+
+    getRole() {
+        return('Intern')
+    }
+
+    getSchool() {
+        return(this.school)
+    }
+}
+
+
+module.exports = Intern;
